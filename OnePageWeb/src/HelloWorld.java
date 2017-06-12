ae56334d3077a4282db2137b1f5f57bd3b9e69ff
meDebasis <pingdevp@gmail.com>
2017-06-12 14:58:17 -0700

    uploading to devlop

--- OnePageWeb/src/HelloWorld.java
+++ OnePageWeb/src/HelloWorld.java
@@ -1,15 +1,30 @@
 
 public class HelloWorld {
 
-	public String name = "Deb";
-
-	public String getName() {
-		return name;
+	public String country = "";
+	public String getCountry() {
+		return country;
 	}
-
-	public void setName(String name) {
-		this.name = name;
+	public void setCountry(String country) {
+		this.country = country;
 	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String state = "";
+	public String city = "";
+	
+	
+	
 	
 	
 }
